7b062f18b48d6336c6148625cbb553f001f0b914
sunattah <sundayattah319@gmail.com>
2026-03-27 07:06:44 +0100

    Create answer.go

---
+++ answer.go
@@ -0,0 +1,49 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+func add(a, b int) int {
+
+}
+
+// func add(a, b int) int {
+
+	// return a + b
+// }
+
+func main() {
+	//fmt.Println(add(2, 3))
+	// var num1 int
+	// var num2 int
+	 var operator string
+	
+
+	// fmt.Println("Enter value: ")
+	// fmt.Scanln(&num1)
+
+	// fmt.Println("Enter value: ")
+	// fmt.Scanln(&num2)
+    
+   
+
+//  add <a> <b>   → addition
+//  sub <a> <b>   → subtraction
+//  mul <a> <b>   → multiplication
+//  div <a> <b>   → division
+
+	// fmt.Println("")
+	// n, err := fmt.Scan(os.Stdin)
+	// if err != nil {
+	// 	fmt.Println("Error not present: ", err)
+	// 	return
+	// }
+	//fmt.Println("can be calculated: ", n)
+	switch operator {
+	case:
+		fmt.Println(num1, num2)
+	 }
+
+}
